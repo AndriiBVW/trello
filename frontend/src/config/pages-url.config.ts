593969b5aa52f091402c9f077d58a5bb178fc618
Andrew Babenko <a.babenko@dinamicka.com>
2024-05-30 17:11:39 +0300

    feat: base setup project

---
+++ frontend/src/config/pages-url.config.ts
@@ -0,0 +1,12 @@
+class DASHBOARD {
+  private root = '/i'
+
+  HOME = this.root
+  TASKS = `${this.root}/tasks`
+  HABITS = `${this.root}/habits`
+  TIMER = `${this.root}/timer`
+  TIME_BLOCKING = `${this.root}/time_blocking`
+  SITTINGS = `${this.root}/sittings`
+}
+
+export const DASHBOARD_PAGES = new DASHBOARD()
