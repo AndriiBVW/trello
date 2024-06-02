aa835c05c2d070fef1999d7eb937b8c5e026f51c
AndriiBVW <andriibvw@gmail.com>
2024-06-02 22:02:45 +0300

    feat: create auth

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.enableCors({
-    origin: ['http://localhost:3001'],
+    origin: ['http://localhost:3000'],
     credentials: true,
     exposedHeaders: ['set-cookie'],
   });
