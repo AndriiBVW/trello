0148688cf1afd86285d6a1f15e1bc599a47b40a2
AndriiBVW <andriibvw@gmail.com>
2024-06-08 12:06:45 +0300

    feat: create time blocking

---
+++ frontend/src/app/i/time-blocking/calc-hours-left.ts
@@ -0,0 +1,9 @@
+import { ITimeBlockResponse } from '@/types/time-block.types'
+
+export function calcHoursLeft(items: ITimeBlockResponse[] | undefined) {
+	const totalMinutes = items?.reduce((acc, item) => acc + item.duration, 0) || 0
+	const totalHours = Math.floor(totalMinutes / 60) // Убедитесь, что деление выполняется правильно
+	const hoursLeft = 24 - totalHours // Вычисляем оставшиеся часы
+
+	return { hoursLeft }
+}
